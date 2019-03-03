549c5e68d361ee5632da45bd60e6229a4f583043
mark <mark@ubuntu.ubuntu-domain>
2019-03-03 23:52:39 +0800

    HW01

--- main.c
+++ main.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
+
+int (*calcarray[])(int, int) =
+{ 
+    plus,
+    minus, 
+    multiply, 
+    divided 
+};
+const char *operation[] =
+{
+    "+",
+    "-",
+    "*",
+    "/"
+};
 int main()
 {
 	int a, c;
-	char b;
-	printf("key a Function \nEX: 1 + 1\n");
+	char b[] = "";
+	printf("key a Function \nEX: 1+1\n");
 	scanf("%i %c %i", &a, &b, &c);
-	//IF ELSE IF
-	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
-	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
-	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
-	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+    for (int i = 0;i<4;i++)
+    {
+        if(strcmp(b,operation[i]) == 0)
+        {
+            printf("%d %s %d = %d\n", a, b, c, calcarray[i](a, c));
+            break;
+        }
+    }
 }
